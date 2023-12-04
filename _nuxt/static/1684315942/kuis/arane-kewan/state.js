457c9325fd6cb4cc4b67fe63439286b4f98fc62a
adityarahmanda <adityarahmanda@users.noreply.github.com>
2023-12-04 14:21:37 +0000

    deploy: fa625ffba240338bc35f8cb4c3dfc0d2e93fb674

--- _nuxt/static/1684315942/kuis/arane-kewan/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmacaksara\u002F_nuxt\u002Fstatic\u002F1684315942",layout:"quiz",error:a,serverRendered:true,routePath:"\u002Fkuis\u002Farane-kewan",config:{_app:{basePath:"\u002Fmacaksara\u002F",assetsPath:"\u002Fmacaksara\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8b0e2112"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1701699695/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmacaksara\u002F_nuxt\u002Fstatic\u002F1701699695",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmacaksara\u002F",assetsPath:"\u002Fmacaksara\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"afd4e33a"}}}}(null));
