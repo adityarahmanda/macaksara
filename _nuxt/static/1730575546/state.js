dce29e4ae5b64e1c7565cbe5175f794084e9397c
adityarahmanda <adityarahmanda@users.noreply.github.com>
2024-11-02 19:36:35 +0000

    deploy: 7ef0649b306dc1cddaf025433cf404c6254b379a

--- _nuxt/static/1730575546/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmacaksara\u002F_nuxt\u002Fstatic\u002F1730575546",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmacaksara\u002F",assetsPath:"\u002Fmacaksara\u002F_nuxt\u002F",cdnURL:a}}}}(null));
