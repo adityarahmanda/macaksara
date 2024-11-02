c5ec2dc0f54211e141b43ab0e00b55f01270c1fe
Aditya Rahmanda <arahmanda21@gmail.com>
2024-11-03 04:42:55 +0700

    fix footer link

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer id="footer" class="row justify-content-center mt-auto py-3">
         <div id="copyright">
-            &copy;2021 - <a class="copyright" href="https://adityarahmanda.com">Aditya Rahmanda</a>
+            &copy;2021 - <a class="copyright" href="https://adityarahmanda.github.io" style="text-decoration: underline;">Aditya Rahmanda</a>
         </div>
     </footer>
 </template>
