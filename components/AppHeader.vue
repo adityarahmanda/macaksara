26c929d039bf2b8135d5bffd47ac16b588672e0d
Aditya Rahmanda <arahmanda21@gmail.com>
2024-11-04 18:39:05 +0700

    fix header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,23 +1,25 @@
 <template>
-    <!-- <b-navbar toggleable="md" class="py-3 mb-3 mb-md-4">
+    <nav class="navbar py-3 mb-3 mb-md-4 navbar-light navbar-expand-md">
         <a target="_self" :href="titleLink">
             <h1 class="navbar-brand">{{ title }}</h1>
-        </a>
-        <b-navbar-toggle class="d-md-none d-block" target="nav-collapse">
-            <template #default="{ expanded }">
-                <i v-if="expanded" class="fa fa-solid fa-xmark"></i>
-                <i v-else class="fa fa-solid fa-bars"></i>
-            </template>
-        </b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item v-for="item in navItems" :key="item.title" :href="item.link" class="pl-0 pl-md-2">{{ item.title }}</b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar> -->
+        </a> 
+        <button type="button" aria-label="Toggle navigation" class="navbar-toggler d-md-none d-block collapsed" :aria-expanded="expanded" aria-controls="nav-collapse" style="overflow-anchor: none;" @click="toggleExpand">
+            <i v-if="expanded" class="fa fa-solid fa-xmark"></i>
+            <i v-else class="fa fa-solid fa-bars"></i>
+        </button> 
+        <div id="nav-collapse" :class="navCollapseClass">
+            <ul class="navbar-nav ml-auto">
+                <li v-for="item in navItems" :key="item.title" class="nav-item pl-0 pl-md-2">
+                    <a :href="item.link" target="_self" class="nav-link">{{ item.title }}</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 const props = defineProps({
     title: {
         type: String,
@@ -37,4 +39,12 @@ const props = defineProps({
         }
     }
 })
+
+const expanded = ref(false)
+const navCollapseClass = ref('navbar-collapse collapse')
+
+const toggleExpand = () => {
+    expanded.value = !expanded.value
+    navCollapseClass.value = expanded.value ? 'navbar-collapse collapse show' : 'navbar-collapse collapse'
+}
 </script>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -22,7 +22,7 @@
                     <div class="col-8">streak tertinggi</div>
                 </div>
             </div>
-            <button class="reset-button pushable w-100 mb-3 mb-md-4" @click="$bvModal.show('reset-progress-modal')" >
+            <button class="reset-button pushable w-100 mb-3 mb-md-4" @click="showModal" >
                 <span class="front">
                     Atur Ulang Permainan
                 </span>
@@ -30,31 +30,37 @@
             <p class="small">*Data permainan tersimpan secara otomatis pada perangkat yang digunakan.</p>
         </div>
 
-        <!-- <b-modal 
-            id="reset-progress-modal"
-            body-class="text-center"
-            centered 
-            hide-header 
-            @ok="$emit('reset-game')
-        ">
-            Apakah kamu yakin untuk mengatur ulang permainan dan kehilangan semua perkembangan permainan?
-        
-            <template #modal-footer="{ cancel, ok }">
-                <div class="row w-100 no-gutters">
-                    <div class="col-12 col-md-6 pr-0 pr-md-1 mb-2 mb-md-0">
-                        <button class="btn btn-primary text-white rounded-pill w-100" @click="cancel">Batalkan</button>
+        <div v-if="isShowModal" id="_modal_outer_" style="position: absolute; z-index: 1040;">
+            <div id="reset-progress-modal" role="dialog" aria-describedby="_modal_body_" class="modal fade show" aria-modal="true" style="display: block;">
+                <div class="modal-dialog modal-md modal-dialog-centered">
+                    <span tabindex="0"></span>
+                    <div id="_modal_content_" tabindex="-1" class="modal-content">
+                        <div id="_modal_body_" class="modal-body text-center">
+                            Apakah kamu yakin untuk mengatur ulang permainan dan kehilangan semua perkembangan permainan?
+                        </div>
+                        <footer id="_modal_footer_" class="modal-footer">
+                            <div class="row w-100 no-gutters">
+                                <div class="col-12 col-md-6 pr-0 pr-md-1 mb-2 mb-md-0">
+                                    <button class="btn btn-primary text-white rounded-pill w-100" @click="hideModal">Batalkan</button>
+                                </div> 
+                                <div class="col-12 col-md-6 pl-0 pl-md-1">
+                                    <button class="btn btn-danger text-white rounded-pill w-100" @click="resetGameAndHideModal">Atur Ulang</button>
+                                </div>
+                            </div>
+                        </footer>
                     </div>
-                    <div class="col-12 col-md-6 pl-0 pl-md-1">
-                        <button class="btn btn-danger text-white rounded-pill w-100" @click="ok">Atur Ulang</button>
-                    </div>
+                    <span tabindex="0"></span>
                 </div>
-            </template>
-        </b-modal> -->
+            </div>
+            <div id="_modal_backdrop_" class="modal-backdrop"></div>
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+
+const emit = defineEmits(['reset-game'])
 
 const props = defineProps({
     user: {
@@ -69,5 +75,18 @@ const props = defineProps({
     }
 })
 
-const isEditName = ref(false)
+const isShowModal = ref(false)
+
+const showModal = () => {
+    isShowModal.value = true;
+}
+
+const hideModal = () => {
+    isShowModal.value = false;
+}
+
+const resetGameAndHideModal = () => {
+    emit('reset-game')
+    hideModal()
+}
 </script>
