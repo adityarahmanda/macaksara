3cf23e155d3496437d18964f06ffe9d437195035
adityarahmanda <adityarahmanda@users.noreply.github.com>
2024-12-06 16:58:15 +0000

    deploy: eb74931f9f325a3d2d3fd56f4dad2ebe0debcad3

---
+++ _nuxt/C-TL5a--.js
@@ -0,0 +1,1 @@
+import{l as a}from"./EtJlR253.js";const e={__name:"konverter",setup(t){return a("https://adityarahmanda.github.io/alih-aksara/latin-jawa",{external:!0}),()=>{}}};export{e as default};
---
+++ _nuxt/CUJKaDA8.js
@@ -0,0 +1,1 @@
+import{v as e}from"./EtJlR253.js";const t={__name:"index",setup(s){return e().push("/"),()=>{}}};export{t as default};
